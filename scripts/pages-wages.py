559f5bb7d470d967c4b83de71b3d83daa8fe48cb
mcleancapital <ianmclean@mcleancapital.ca>
2025-01-17 17:01:45 -0500

    Update pages-wages.py

--- scripts/pages-wages.py
+++ scripts/pages-wages.py
@@ -18,7 +18,7 @@ latest_date = data.iloc[-1]["Date"]
 latest_volume = data.iloc[-1]["Value"]
 
 # Format the "let pi" data
-dates_since_reference = (data["Date"] - datetime(1969, 12, 10)).dt.days.tolist()
+dates_since_reference = (data["Date"] - datetime(1970, 1, 1)).dt.days.tolist()
 monthly_totals = data["Value"].tolist()
 pi_data = f"let pi = [{dates_since_reference}, {monthly_totals}, null, null, '%', 0, []];"
 
